f7cc5593b40b0f5ceac96e273630ad30107d7ed9
Asher Raz <asherr1211@gmail.com>
2020-08-27 13:22:59 +0300

    Update alternative_gibush.R

--- alternative_gibush.R
+++ alternative_gibush.R
@@ -1603,8 +1603,6 @@ gibush_candidates_kakatz_11.2020_civil<-as.data.frame(gibush_candidates_kakatz_1
 class(gibush_candidates_kakatz_11.2020_civil)
 gibush_candidates_kakatz_11.2020_civil<-gibush_candidates_kakatz_11.2020_civil[order(gibush_candidates_kakatz_11.2020_civil$order_num,na.last=F),]
 
-
-
 #**********************get and share scores***************************
 
 
@@ -1624,7 +1622,7 @@ locale("he")
 
 #**********************get and share scores - loop starts***************************
 
-#run the following rows only once-when building the google sheet.
+#run the following rows only once-when creating the google sheet.
 
 # drive_download("gibush_candidates_kakatz_11.2020_civil_final_scores****",path = "C:/Users/Asher/Documents/MAMDA/JOMAGAV/alternative_gibush/gibush_candidates_kakatz_11.2020_civil_final_scores_gd",type = "csv",overwrite=T)
 # gibush_candidates_kakatz_11.2020_civil_final_scores_gd<-read_csv("C:/Users/Asher/Documents/MAMDA/JOMAGAV/alternative_gibush/gibush_candidates_kakatz_11.2020_civil_final_scores_gd.csv")
@@ -1661,8 +1659,6 @@ locale("he")
 # End of code for sharing the google sheet
 
 n=0
-gibush_candidates_kakatz_11.2020_civil<-gibush_candidates_kakatz_11.2020_civil[order(gibush_candidates_kakatz_11.2020_civil$order_num,na.last=F),]
-
 repeat {
   drive_auth(email = "asherr1211@gmail.com")
   drive_download("gibush_candidates_kakatz_11.2020_civil_final_scores****",path = "C:/Users/Asher/Documents/MAMDA/JOMAGAV/alternative_gibush/gibush_candidates_kakatz_11.2020_civil_final_scores_gd",type = "csv",overwrite=T)
@@ -1699,16 +1695,15 @@ repeat {
 write.csv(gibush_candidates_kakatz_11.2020_civil_final_scores_gd, file="C:/Users/Asher/Documents/MAMDA/JOMAGAV/alternative_gibush/gibush_candidates_kakatz_11.2020_civil_final_scores_gd.csv")
 drive_update("https://docs.google.com/spreadsheets/d/1n0pmKCYDixljzyBJuh_YuyshwCZVlY-7v7Uq7HgptHc/edit#gid=0", "C:/Users/Asher/Documents/MAMDA/JOMAGAV/alternative_gibush/gibush_candidates_kakatz_11.2020_civil_final_scores_gd.csv")
 
-
   timestamp()
   
   n <- n+1
-  if (n == 3){   # set the n(n times the code should run)
+  if (n == 3){ # set the n(n times the code should run)
     break
-  }
   
 Sys.sleep(600)  # set the time (in sec.)
 
+  }
 }
 
 
